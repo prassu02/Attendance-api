ceb30696d0b79d3cbc60b71fbd8768d0efc5df01
Prasanna kumar Kommiri <142135618+prassu02@users.noreply.github.com>
2026-04-30 23:23:48 +0530

    Update auth.py

--- src/routers/auth.py
+++ src/routers/auth.py
@@ -1,25 +1,12 @@
-from fastapi import APIRouter, Depends, HTTPException
-from sqlalchemy.orm import Session
-from src.database import SessionLocal
-from src.models import User
-from src.auth import hash_password, verify_password, create_token
+from fastapi import APIRouter
+from src.utils.auth import hash_password, verify_password, create_token
 
-router = APIRouter(prefix="/auth")
+router = APIRouter()
 
 @router.post("/signup")
-def signup(email: str, password: str, role: str):
-    db: Session = SessionLocal()
-    user = User(email=email, password=hash_password(password), role=role)
-    db.add(user)
-    db.commit()
-    return {"msg": "created"}
+def signup():
+    return {"message": "Signup working"}
 
 @router.post("/login")
-def login(email: str, password: str):
-    db: Session = SessionLocal()
-    user = db.query(User).filter(User.email == email).first()
-
-    if not user or not verify_password(password, user.password):
-        raise HTTPException(401)
-
-    return {"token": create_token({"user_id": user.id, "role": user.role})}
+def login():
+    return {"message": "Login working"}
