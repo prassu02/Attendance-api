451a067a9a52b0948624f907ada61975f15be291
Prasanna kumar Kommiri <142135618+prassu02@users.noreply.github.com>
2026-05-01 00:22:02 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+# IMPORT ROUTERS
 from src.routes.auth import router as auth_router
 from src.routes.sessions import router as session_router
 from src.routes.attendance import router as attendance_router
@@ -11,6 +12,7 @@ from src.routes.monitoring import router as monitoring_router
 def home():
     return {"message": "API is running"}
 
+# INCLUDE ROUTERS
 app.include_router(auth_router, prefix="/auth", tags=["Auth"])
 app.include_router(session_router, prefix="/sessions", tags=["Sessions"])
 app.include_router(attendance_router, prefix="/attendance", tags=["Attendance"])
